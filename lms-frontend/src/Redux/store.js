a1fd1ef42538fa29ff5d6035b8b13928b45530b6
swarnkarvikash <vikashswarnkar3@gmail.com>
2025-04-20 12:07:43 +0530

    Started adding basic redux setup with toolkit

--- lms-frontend/src/Redux/store.js
+++ lms-frontend/src/Redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authSliceReducer from './Slices/AuthSlice';
+
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authSliceReducer
+    },
     devTools: true
 });
 
