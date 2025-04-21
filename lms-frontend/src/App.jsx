da45e6277073dec30b52bc3e29b43236fe630ed1
swarnkarvikash <vikashswarnkar3@gmail.com>
2025-04-22 01:14:27 +0530

    Added aboutUs page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
+import AboutUs from './Pages/AboutUs';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
   return(
     <>
       <Routes>
-         <Route path="/" element={<HomePage />}> </Route> */}
+         <Route path="/" element={<HomePage />}> </Route>
+         <Route path="/about" element={<AboutUs />}> </Route>
       </Routes>
     </>
   )
--- lms-frontend/src/Layouts/HomeLayout.jsx
+++ lms-frontend/src/Layouts/HomeLayout.jsx
@@ -51,7 +51,7 @@ function HomeLayout({ children }) {
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className='drawer-overlay'>
                     </label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <ul className="menu p-4 w-48 h-[100%]  sm:w-80 bg-base-100 text-base-content relative">
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24}></AiFillCloseCircle>
@@ -80,17 +80,17 @@ function HomeLayout({ children }) {
                         {!isLoggedIn && (
                             <li className='relative bottom-4 w-[90%]'>
                             <div className="w-full flex items-center justify-center">
-                                <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full bg-blue-600'>
                                     <Link to="/login">Login</Link>
                                 </button>
-                                <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full bg-pink-600'>
                                     <Link to="/login">Signup</Link>
                                 </button>
                             </div>
                             </li>
                         )}
 
-                    {!isLoggedIn && (
+                    {isLoggedIn && (
                             <li className='relative bottom-4 w-[90%]'>
                             <div className="w-full flex items-center justify-center">
                                 <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
